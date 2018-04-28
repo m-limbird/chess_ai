0a8acf611694f7b4e0fc90ecb157f27343da8131
Mike Limbird <m-limbird@onu.edu>
2018-04-28 18:54:51 -0400

    fixed untracked files

--- devel/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mike/Documents/chess_ai/devel/googletest-src
-# Build directory: /home/mike/Documents/chess_ai/devel/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(googlemock)
--- devel/googletest-build/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mike/Documents/chess_ai/devel/googletest-src/googletest
-# Build directory: /home/mike/Documents/chess_ai/devel/googletest-build/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
